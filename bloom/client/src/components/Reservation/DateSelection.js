04fd50277118d766c6d589ab86c6c311aa5c8fe4
Arthur Kasumyan <arthurkasumyan@gmail.com>
2020-03-16 13:32:11 -0700

    basic reservation outline
    finish reservation views
    
    basic appointment time selection with lots of assumptions, some bugs
    
    fix packaging and ignore packagelocks

---
+++ bloom/client/src/components/Reservation/DateSelection.js
@@ -0,0 +1,192 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import './DateSelection.css';
+import { Form, Button } from 'react-bootstrap';
+
+
+class DateSelection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: new Date(),
+      selectedTime: 540,
+      storeHours: [{ day: 1, start_time: 540, end_time: 960 }, { day: 2, start_time: 540, end_time: 660 },
+      { day: 3, start_time: 540, end_time: 1020 }, { day: 4, start_time: 540, end_time: 1020 }, { day: 5, start_time: 540, end_time: 1020 },
+      { day: 6, start_time: 540, end_time: 1020 }, { day: 7, start_time: 540, end_time: 960 }, { day: 8, start_time: 540, end_time: 960 },
+      { day: 9, start_time: 540, end_time: 1020 }, { day: 10, start_time: 540, end_time: 1020 }, { day: 11, start_time: 540, end_time: 1020 },
+      { day: 12, start_time: 540, end_time: 1020 }, { day: 13, start_time: 540, end_time: 1020 }, { day: 14, start_time: 540, end_time: 960 },
+      { day: 15, start_time: 540, end_time: 960 }, { day: 16, start_time: 540, end_time: 1020 }, { day: 17, start_time: 540, end_time: 1020 },
+      { day: 18, start_time: 540, end_time: 1020 }, { day: 19, start_time: 540, end_time: 1020 }, { day: 20, start_time: 540, end_time: 1020 }, { day: 21, start_time: 540, end_time: 960 },
+      { day: 22, start_time: 540, end_time: 960 }, { day: 23, start_time: 540, end_time: 1020 }, { day: 24, start_time: 540, end_time: 1020 },
+      { day: 25, start_time: 540, end_time: 1020 }, { day: 26, start_time: 540, end_time: 1020 }, { day: 27, start_time: 540, end_time: 1020 },
+      { day: 28, start_time: 540, end_time: 960 }, { day: 29, start_time: 540, end_time: 960 }, { day: 30, start_time: 540, end_time: 1020 }, { day: 31, start_time: 540, end_time: 1020 }
+      ],
+      workersSchedules: [{ worker_id: 1, schedule: [{ day: 19, booked_slots: [{ start_time: 0, end_time: 120 }], hours: { start_time: 540, end_time: 1020 } }] },
+      {
+        worker_id: 2, schedule: [{ day: 19, booked_slots: [{ start_time: 120, end_time: 240 }], hours: { start_time: 540, end_time: 1020 } },
+        { day: 20, booked_slots: [{ start_time: 240, end_time: 360 }], hours: { start_time: 540, end_time: 1020 } }
+        ]
+      },
+      ],
+    };
+  }
+
+  handleDateChange = date => {
+    this.setState({
+      startDate: date
+    });
+  };
+
+  handleSelectChange = (event) => {
+    this.setState({
+      selectedTime: parseInt(event.target.value)
+    });
+  };
+
+  convertMinsToHrsMins(mins) {
+    let h = Math.floor(mins / 60);
+    let m = mins % 60;
+    let am = false
+    if (h <= 12) {
+      am = true
+    } else {
+      h -= 12
+    }
+    h = h < 10 ? '0' + h : h;
+    m = m < 10 ? '0' + m : m;
+    if (am) {
+      return `${h}:${m}am`;
+    } else {
+      return `${h}:${m}pm`;
+    }
+
+  }
+
+  render() {
+    const CreateTimeSelects = (props) => {
+      let items = [];
+      for (let i = this.state.storeHours[this.state.startDate.getDate() - 1].start_time; i + this.props.time <= this.state.storeHours[this.state.startDate.getDate() - 1].end_time; i += 60) {
+        items.push(<option key={i} value={i}>{this.convertMinsToHrsMins(i)}</option>);
+      }
+      return items;
+    }
+
+    const SlotsAtSelectedTime = () => {
+      // console.log('here')
+      // console.log(this.state.selectedTime)
+      let slots = []
+      let schedules = []
+      for(let i = this.state.selectedTime; (i < this.state.selectedTime + 120 && i + this.props.time <= this.state.storeHours[this.state.startDate.getDate()-1].end_time); i += 15) {
+        let currTime = i
+        let foundSchedule = false
+        let currSchedule = []
+        // console.log(this.props.selectedServices)
+        for(let j = 0; j < this.props.selectedServices.length; j++) {
+          let currService = this.props.selectedServices[j]
+          let foundSlot = false
+          // console.log('schedules look like')
+          // console.log(this.state.workersSchedules)
+          for(let k = 0; k < this.state.workersSchedules.length; k++) {
+            let available = false
+            let currWorker = this.state.workersSchedules[k].worker_id
+            for(let l = 0; l < this.state.workersSchedules[k].schedule.length; l++) {
+              let currDaySchedule = this.state.workersSchedules[k].schedule[l]
+              // console.log('currDaySchedule is: ', currDaySchedule.day)
+              // console.log('startDate is: ', this.state.startDate.getDate())
+              if(currDaySchedule.day == this.state.startDate.getDate()) {
+                for(let m = 0; m < currDaySchedule.booked_slots.length; m++) {
+                  let currBookedSlotStartTime = currDaySchedule.booked_slots[m].start_time + currDaySchedule.hours.start_time
+                  let currBookedSlotEndTime = currDaySchedule.booked_slots[m].end_time + currDaySchedule.hours.start_time
+                  // console.log('CurrWorker', currWorker)
+                  // console.log('CurrTime', currTime)
+                  // console.log('currBookedSlotStartTime', currBookedSlotStartTime)
+                  // console.log('currBookedSlotEndTime', currBookedSlotEndTime)
+                  // console.log('CurrDaySchedule booked from: ', currBookedSlotStartTime, ' to: ', currBookedSlotEndTime)
+                  if((currTime >= currBookedSlotStartTime && currTime <= currBookedSlotEndTime) || (currTime + currService.duration >= currBookedSlotStartTime && currTime + currService.duration <= currBookedSlotEndTime)) {
+                    // console.log('Unavailable')
+                    break
+                  } else {
+                    available = true
+                  }
+                }
+                // console.log('out innest loop')
+                if(available) {
+                  // console.log("&&&&&&&&&&&&&&&&&")
+                  currSchedule.push({worker_id: currWorker, service_id: currService.service_id, start_time: currTime, end_time: currTime+currService.duration, price: currService.cost, date: this.state.startDate})
+                  currTime += currService.duration
+                }
+                break
+                
+              }
+            }
+            if(available) {
+              foundSlot = true
+              break
+            }
+          }
+          if(foundSlot) {
+            foundSchedule = true
+            break
+          }
+        }
+        if(foundSchedule) {
+          // console.log('pushing schedule: ', currSchedule)
+          schedules.push(currSchedule)
+          slots.push(<Button className="mt-3 mx-2" key={i}>{this.convertMinsToHrsMins(i)}</Button>)
+        }
+      }
+      if(slots.length == 0) {
+        return <h1>No available appointments</h1>
+      }
+      // console.log('returning slots')
+      // console.log('                 ')
+      // console.log('                 ')
+      // console.log('                 ')
+      // console.log('                 ')
+      // console.log('                 ')
+      return slots
+    }
+
+    return (
+      <Card
+        text='dark'
+        className='mt-0 py-3'
+      >
+        <h3>Select Appointment Time</h3>
+        <Card.Body className='pt-0'>
+          <Row className='justify-content-center'>
+          <Col xs="11" md="6" className="mt-3">
+              <div className="customDatePickerWidth">
+              <DatePicker
+                className="form-control"
+                selected={this.state.startDate}
+                onChange={this.handleDateChange}
+                minDate={new Date()}
+              />
+              </div>
+            </Col>
+            <Col xs="11" md="6" className="mt-3">
+              <Form>
+                  <Form.Control as="select" value={this.state.selectedTime} onChange={this.handleSelectChange.bind(this)}>
+                    <CreateTimeSelects date={this.state.startDate} />
+                  </Form.Control>
+              </Form>
+            </Col>
+          </Row>
+          <Row className="justify-content-center text-xs-center text-sm-left pl-2">
+            <Col xs="12" md="11" className="px-1">
+              {/* Maybe want to have this with multiple rows, each row belongs to technician. One last row is technician mix to make the appointment work out */}
+              <SlotsAtSelectedTime />
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
+
+export default DateSelection;
--- bloom/client/src/components/Reservation/ReservationPage.js
+++ bloom/client/src/components/Reservation/ReservationPage.js
@@ -1,25 +1,176 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import ReservationForm from './ReservationForm';
+import ServiceSelection from './ServiceSelection'
+import './ReservationPage.css'
+import { Row, Col, Card, ListGroup } from 'react-bootstrap'
+import { FaArrowLeft } from 'react-icons/fa'
+import DateSelection from './DateSelection';
 
-class Homepage extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+class ReservationPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      storeName: 'Nails R Us',
+      total: 0,
+      time: 0,
+      currentStep: 1,
+      selectedServices: []
+    };
+  }
+
+  updateReservation = (removal, currService) => {
+    if (removal) {
+      this.setState({ time: this.state.time - currService.duration })
+      this.setState({ total: this.state.total - currService.cost })
+      this.setState({
+        selectedServices: this.state.selectedServices.filter(function (selectedService) {
+          return currService.id !== selectedService.id;
+        }
+        )
+      })
+    } else {
+      this.setState({ time: this.state.time + currService.duration })
+      this.setState({ total: this.state.total + currService.cost })
+      this.setState({ selectedServices: [...this.state.selectedServices, currService] })
+    }
+  }
+
+  backStep = (event) => {
+    var newStep = this.state.currentStep - 1
+    this.setState({
+      currentStep: newStep
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    if (this.state.currentStep < 4) {
+      var newStep = this.state.currentStep + 1
+      this.setState({
+        currentStep: newStep
+      })
+    } else {
+      alert(JSON.stringify(this.state));
+    }
+  }
+
+  pluralize = (val, word, plural = word + 's') => {
+    const _pluralize = (num, word, plural = word + 's') =>
+      [1, -1].includes(Number(num)) ? word : plural;
+    if (typeof val === 'object') return (num, word) => _pluralize(num, word, val[word]);
+    return _pluralize(val, word, plural);
+  };
+
+  timeConvert = (n) => {
+    var num = n;
+    var hours = (num / 60);
+    var rhours = Math.floor(hours);
+    var minutes = (hours - rhours) * 60;
+    var rminutes = Math.round(minutes);
+    return rhours + " " + this.pluralize(rhours, 'hour') + " and " + rminutes + " " + this.pluralize(rminutes, 'minute');
+  }
+
+
 
   render() {
+    let that = this;
+    const DisplayByStep = (props) => {
+      if (this.state.currentStep == 1) {
+        return <ServiceSelection updateReservation={this.updateReservation} selectedServices={this.state.selectedServices} time={this.state.time} total={this.state.total} handleSubmit={this.handleSubmit} timeConvert={this.timeConvert} pluralize={this.pluralize} />
+      } else {
+        return <DateSelection time={this.state.time} selectedServices={this.state.selectedServices}/>
+      }
+    }
+
+    const DisplayBackButton = (props) => {
+      if (this.state.currentStep == 1) {
+        return null
+      } else {
+        return <FaArrowLeft size={'2em'} className="pt-2 pr-2" onClick={this.backStep} />
+      }
+    }
+
+    function ServiceList(props) {
+      if (props.services) {
+        const servicesList = props.services.map((service) => {
+          return <ListGroup.Item variant="light" key={service.name}>
+            <Row>
+              <Col lg={7}>
+                <Row>
+                  {service.name}
+                </Row>
+                <Row className="smallText">
+                  {service.duration} {that.pluralize(service.duration, 'minute')}
+                </Row>
+              </Col>
+              <Col lg={5}>
+                <div className="float-right">
+                  ${service.cost.toFixed(2)}
+                </div>
+              </Col>
+            </Row>
+          </ListGroup.Item>;
+        });
+
+        return (
+          <ListGroup variant='flush'>{servicesList}</ListGroup>
+        );
+      }
+      return null
+    }
+
     return (
       <Container fluid>
-        <Row className="justify-content-center">
-          <Col xs={12} sm={11} md={10} lg={9}>
-            <ReservationForm/>
+        <Row noGutters className="pt-3 pb-0">
+          <Col xs="1">
+            <DisplayBackButton />
+          </Col>
+          <Col xs="11" className="text-left">
+            <h2>Step {this.state.currentStep}</h2>
           </Col>
         </Row>
+
+        <Row>
+          <Col xs={12} lg={8} className="largeMarginBottom">
+            <DisplayByStep />
+          </Col>
+          <Col xs={12} lg={4} className="d-none d-lg-block">
+            <Card
+              text='dark'
+              className='shoppingCart mt-0'
+            >
+              <Card.Header>Shopping Cart</Card.Header>
+              <Card.Body className='pt-0'>
+                <Row className='text-left'>
+                  <Col>
+                    <ServiceList services={this.state.selectedServices} />
+                  </Col>
+                </Row>
+
+                <h2>Total: ${this.state.total.toFixed(2)}</h2>
+                <h2>Time: {this.state.time} minutes</h2>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={12} className="d-lg-none shoppingCartSmall px-0">
+            <Card
+              bg='light'
+              text='dark'
+            >
+              <Card.Header className='py-1'>Shopping Cart</Card.Header>
+              <Card.Body className="smallPadding">
+                <h6>{this.state.selectedServices.length} Selected {this.pluralize(this.state.selectedServices.length, 'Service')}</h6>
+                <h6>Total: ${this.state.total.toFixed(2)}</h6>
+                <h6>Time: {this.timeConvert(this.state.time)}</h6>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+
+
       </Container>
     );
   }
 }
 
-export default Homepage;
+export default ReservationPage;
