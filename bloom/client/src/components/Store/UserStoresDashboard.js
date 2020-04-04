b7d1a94d78b8760c410126e376ca814babe4d168
roulasharqawe <roula.sharqawe@yale.edu>
2020-04-04 14:38:23 -0400

    style for store dashboard

--- bloom/client/src/components/Store/UserStoresDashboard.js
+++ bloom/client/src/components/Store/UserStoresDashboard.js
@@ -4,11 +4,23 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { Button } from 'react-bootstrap';
 import SearchCard from '../Search/SearchCard';
+import Carousel from 'react-bootstrap/Carousel'
+import salon from '../../assets/salon.jpg';
+import salon2 from '../../assets/salon2.jpeg';
+import hair from '../../assets/hair_salon.jpg';
+import { FaEdit } from 'react-icons/fa';
 import { withRouter } from "react-router-dom";
 import {
   addAlert
 } from '../../reduxFolder/actions'
 import store from '../../reduxFolder/store';
+
+// ***** NOTE: Change those with this.state.category, this.state.street etc
+
+const categories = ['Nail Salon', 'Hair Salon', 'Spa & Wellness Center'];
+const street = "237 Clybourne Avenue";
+const city = "Los Angeles";
+const state = "CA";
 
 class UserStoresDashboard extends React.Component {
   constructor(props) {
@@ -112,30 +124,59 @@ class UserStoresDashboard extends React.Component {
             stores: data,
           })
         }
-        
+
       });
     }
   }
 
+  // {this.state.stores.map(store => (
+  //   <Col key={"store-" + store.id}>
+  //     <Button onClick={() => this.triggerStoreEdit(store)}>Edit Store</Button>
+  //     <Button onClick={() => this.triggerShowWorkers(store.id)}>Show Workers</Button>
+  //     <Button onClick={() => this.triggerAddWorker(store.id)}>Add Worker</Button>
+  //     <Button onClick={() => this.triggerShowServices(store.id)}>Show Services</Button>
+  //     <Button onClick={() => this.triggerAddService(store.id)}>Add Service</Button>
+  //     <SearchCard store={store}
+  //                 carousel={true}
+  //                 styleVal={{ width: '18rem' }}
+  //                 omitBookOption={true}
+  //                 onClickFunctionStore={() => this.triggerStoreShow(store)}/>
+  //   </Col>
+  // ))}
+
   render() {
     return (
       <Container fluid>
+        <div className="style_column"> </div>
+        <FaEdit className="edit" size={25} />
         <Row className="justify-content-center">
-          {this.state.stores.map(store => (
-            <Col key={"store-" + store.id}>
-              <Button onClick={() => this.triggerStoreEdit(store)}>Edit Store</Button>
-              <Button onClick={() => this.triggerShowWorkers(store.id)}>Show Workers</Button>
-              <Button onClick={() => this.triggerAddWorker(store.id)}>Add Worker</Button>
-              <Button onClick={() => this.triggerShowServices(store.id)}>Show Services</Button>
-              <Button onClick={() => this.triggerAddService(store.id)}>Add Service</Button>
-              <SearchCard store={store} 
-                          carousel={true} 
-                          styleVal={{ width: '18rem' }}
-                          omitBookOption={true}
-                          onClickFunctionStore={() => this.triggerStoreShow(store)}/>
-            </Col>
-          ))}
+
+          <Carousel className="dashboard_carousel" interval="">
+
+              <Carousel.Item className="item">
+                <img className="item" src={salon2} />
+              </Carousel.Item>
+              <Carousel.Item className="item">
+                <img className="item" src={hair} />
+              </Carousel.Item>
+
+          </Carousel>
+          <Col style={{marginLeft: 450, marginTop: 100}}>
+            <p className="name"> Salon Name </p>
+              <p className="address">{street}, {city}, {state} </p>
+            <div style={{marginLeft:200}}>
+            {categories.map(category => (
+            <div className="category"> <p style={{color: 'white'}}> {category} </p> </div>
+            ))}
+            </div>
+
+            <div style={{width: 500, marginLeft: 330}}>
+            <p className="description"> Description: Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+          </Col>
         </Row>
+
       </Container>
     );
   }
