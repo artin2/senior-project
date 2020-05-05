428ca046dda8449c29ac87d177c74a9a45ab47d3
artin2 <artinkasumyan@gmail.com>
2020-05-05 01:56:04 -0700

    Update store display page

--- bloom/client/src/components/Store/StoreDisplay.js
+++ bloom/client/src/components/Store/StoreDisplay.js
@@ -12,6 +12,9 @@ import {
 import store from '../../reduxFolder/store';
 import './StoreDisplay.css'
 import { getPictures } from '../s3'
+import {ListGroup} from 'react-bootstrap'
+import { FaEdit } from 'react-icons/fa';
+
 const fetchDomain = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_FETCH_DOMAIN_PROD : process.env.REACT_APP_FETCH_DOMAIN_DEV;
 
 class StoreDisplay extends React.Component {
@@ -34,8 +37,35 @@ class StoreDisplay extends React.Component {
         lat: "",
         lng: ""
       },
-      pictures: []
+      pictures: [],
+      daysOfWeek: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     }
+  }
+
+  convertMinsToHrsMins(mins) {
+    let h = Math.floor(mins / 60);
+    let m = mins % 60;
+    let am = false
+    if (h == 24) {
+      am = true
+      h -= 12
+    }
+    else if (h < 12) {
+      am = true
+    } else if (h != 12) {
+      h -= 12
+    }
+    h = h < 10 ? '0' + h : h;
+    if (h == 0) {
+      h = '12'
+    }
+    m = m < 10 ? '0' + m : m;
+    if (am) {
+      return `${h}:${m}am`;
+    } else {
+      return `${h}:${m}pm`;
+    }
+
   }
 
   triggerStoreEdit() {
@@ -70,6 +100,37 @@ class StoreDisplay extends React.Component {
           key: "/salon.jpg"
         }
       ]
+    }
+
+    if(this.props.location.state && this.props.location.state.storeHours){
+      this.setState({
+        storeHours: this.props.location.state.storeHours,
+      })
+    }
+    else{
+      fetch(fetchDomain + '/stores/' + this.props.match.params.store_id + '/storeHours', {
+        method: "GET",
+        headers: {
+          'Content-type': 'application/json'
+        },
+        credentials: 'include'
+      })
+      .then(function(response){
+        if(response.status!==200){
+          // throw an error alert
+          store.dispatch(addAlert(response))
+        }
+        else{
+          return response.json();
+        }
+      })
+      .then(data => {
+        if(data){
+          this.setState({
+            storeHours: data,
+          })
+        }
+      });
     }
 
     if(this.props.location.state && this.props.location.state.store){
@@ -117,36 +178,51 @@ class StoreDisplay extends React.Component {
   render() {
     let editButton;
     if(Cookies.get('user') && this.state.store.owners.indexOf(JSON.parse(Cookies.get('user').substring(2)).id) > -1){
-      editButton = <Button className="float-left" onClick={() =>  this.triggerStoreEdit()}>Edit Store</Button>
+      editButton = <FaEdit className="edit mb-3" style={{marginTop: "40px"}} size={25} onClick={() => this.triggerStoreEdit()}/>
+    }
+
+    const ListWorkingHours = (props) => {
+      if(props.storeHours){
+        let items = [];
+        for (let i = 0; i < props.storeHours.length; i++) {
+          if (props.storeHours[i].open_time != null) {
+            items.push(<Col sm="11" md="10" key={i} style={{backgroundColor: "#bdcddb"}}><ListGroup.Item className={"py-1"} style={{backgroundColor: "#bdcddb"}}>{this.state.daysOfWeek[i]}: {this.convertMinsToHrsMins(props.storeHours[i].open_time)}-{this.convertMinsToHrsMins(props.storeHours[i].close_time)}</ListGroup.Item></Col>);
+          }
+          else {
+            items.push(<Col sm="11" md="10" key={i} style={{backgroundColor: "#bdcddb"}}><ListGroup.Item className={"py-1"} style={{backgroundColor: "#bdcddb"}}>{this.state.daysOfWeek[i]}: Off</ListGroup.Item></Col>);
+          }
+        }
+        return items;
+      }
+      return null
     }
 
     return (
       <Container fluid>
         <Row className="justify-content-md-center" style={{ marginTop: '15px', marginBottom: '15px'}}>
-          <Col lg={3}>
-            <h1>{this.state.store.name}</h1>
-            <h5 style={{color: "gray"}}>{this.state.store.address}</h5>
-            <hr/>
-            <p style={{fontSize: "25px"}}>{this.state.store.description}</p>
-            <ul>
-              {this.state.store.category.map((cat, index) => (
-                <li key={"cat-" + index}>{cat}</li>
-              ))}
-            </ul>
-            <div id="buttonGroup">
-              {editButton}
-              <Button className="float-left" onClick={() =>  this.triggerBook()}>Book Now</Button>
-            </div>
-          </Col>
-          <Col xs={10} sm={9} md={8} lg={7}>
+          <Col md={6}>
             <LargeCarousel className="carousel" pictures={this.state.store.pictures}/>
           </Col>
+
+          <Col md={5}>
+            <Row className={"justify-content-center"}>
+              <p className="name">{this.state.store.name}</p>
+              {editButton}
+            </Row>
+            <p className="address">{this.state.store.address}</p>
+            <hr/>
+            <ListGroup variant="flush" style={{marginTop: "-30px"}}>
+              <Row className="justify-content-center mt-4">
+                <h5>Store Hours</h5>
+                <ListWorkingHours storeHours={this.state.storeHours}/>
+              </Row>
+            </ListGroup>
+          </Col>
         </Row>
-        <Row>
-          {/* <WorkerDisplay/> */}
-        </Row>
-        <Row>
-          {/* <ServiceDisplay/> */}
+        <Row style={{marginTop: "20px", marginLeft: "25px"}}>
+          <Col md={6}>
+            <p className="address-large">{this.state.store.description}</p>
+          </Col>
         </Row>
       </Container>
     );
